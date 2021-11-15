62e9768b91a5cfcec12314349a25d13d415d2000
Daniel Rejón <rejonpardenilla@gmail.com>
2021-11-15 11:11:26 -0600

    Fix tests rubocop

--- spec/invisible_record_model_spec.rb
+++ spec/invisible_record_model_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe InvisibleRecord::Model do
   end
 
   it "hides attributes when setting deleted_at attribute" do
-    post = Post.new(title: 'Hello', body: 'World')
-    expect(post.title).to eq('Hello')
-    expect(post.body).to eq('World')
+    post = Post.new(title: "Hello", body: "World")
+    expect(post.title).to eq("Hello")
+    expect(post.body).to eq("World")
     deleted_datetime = DateTime.now
     post.deleted_at = deleted_datetime
     expect(post.deleted_at).to eq(deleted_datetime)
     expect(post).to respond_to(:restore)
     expect(post).to respond_to(:hidden_title)
-    expect(post.hidden_title).to eq('Hello')
-    expect(post.hidden_body).to eq('World')
+    expect(post.hidden_title).to eq("Hello")
+    expect(post.hidden_body).to eq("World")
     expect(post.title).to be_nil
     expect(post.body).to be_nil
   end
